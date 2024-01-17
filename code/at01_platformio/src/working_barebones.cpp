a091410c83fe2dafaf61b345790d6ba84cdf48c1
hshahzada <haashimisna@gmail.com>
2024-01-17 03:25:14 -0500

    adding working code for sensor, screen, project config settings

--- code/at01_platformio/src/working_barebones.cpp
+++ code/at01_platformio/src/working_barebones.cpp
@@ -23,17 +23,24 @@
 #include <Wire.h>
 #include <Adafruit_I2CDevice.h>
 #include "Adafruit_MPRLS.h"
+#include <SPI.h>
+#include  <TFT_eSPI.h> // Loads the library itself
 
 // You dont *need* a reset and EOC pin for most uses, so we set to -1 and don't connect
 #define RESET_PIN  -1  // set to any GPIO pin # to hard-reset on begin()
 #define EOC_PIN    -1  // set to any GPIO pin to read end-of-conversion by pin
 #define SOLENOID_PIN  27
-#define MOTOR_PIN  13
+#define MOTOR_PIN  15
+#define LED_PIN 13
 Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
+TFT_eSPI tft = TFT_eSPI();
+
+float calibrationValue;
 
 void setup() {
   pinMode(MOTOR_PIN, OUTPUT);
   pinMode(SOLENOID_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   digitalWrite(SOLENOID_PIN, HIGH);
   digitalWrite(MOTOR_PIN, LOW);
   Serial.begin(115200);
@@ -41,22 +48,31 @@ void setup() {
   if (! mpr.begin()) {
     Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
     while (1) {
+      digitalWrite(LED_PIN, HIGH);
       delay(10);
     }
   }
   Serial.println("Found MPRLS sensor");
+  calibrationValue = mpr.readPressure();
+
+  // LCD
+  tft.init();
+  tft.fillScreen(0x00);
 }
 
 void loop() {
-  float pressure_hPa = mpr.readPressure();
-  Serial.print("Pressure (hPa): "); Serial.println(pressure_hPa);
-  Serial.print("Pressure (PSI): "); Serial.println(pressure_hPa / 68.947572932);
-  if (pressure_hPa >= 1000){
+  float pressure_mmHg = (mpr.readPressure()-calibrationValue)*0.75;
+  Serial.print("Pressure (mmHg): "); Serial.println(pressure_mmHg);
+  Serial.print("Pressure (PSI): "); Serial.println(pressure_mmHg / 68.947572932);
+  if (pressure_mmHg >= 100){
     digitalWrite(MOTOR_PIN, LOW);
     digitalWrite(SOLENOID_PIN, LOW);
+    digitalWrite(LED_PIN, HIGH);
   } else {
     digitalWrite(MOTOR_PIN, HIGH);
     digitalWrite(SOLENOID_PIN, HIGH);
+    digitalWrite(LED_PIN, LOW);
   }
+  tft.drawString(String(pressure_mmHg), 50, 100, 6);
   delay(1000);
 }
